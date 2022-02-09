ac848b5bb6ce742f4a591fad22f2531dfa84ed27
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-02-09 12:50:16 -0500

    update blink

--- blink_project/blink_project.ino
+++ blink_project/blink_project.ino
@@ -3,15 +3,15 @@ void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
-void const blinkOn(const int d)
+void const blinkOn(const int d) // Turns on LED for duration d
 {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(d);  
+  digitalWrite(LED_BUILTIN, HIGH); // send signal to turn voltage on HIGH for LED at LED_BUILTIN
+  delay(d);
 }
 
-void const blinkOff(const int d)
+void const blinkOff(const int d) // Turns off LED for duration d
 {
-  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LED_BUILTIN, LOW); // send signal to turn voltage on LOW for LED at LED_BUILTIN
   delay(d);  
 }
 
@@ -22,22 +22,16 @@ void const blink(const int d)
 }
 
 void loop() {
-//  int a[3] = {100,250,500}; // array of blink duration values
-//  int b[3] = {5, 3, 1};     // array of blink repetition values
-//  
-//  int i=0; // initialize counter at 0
-//  do
-//    {
-//      for (int j=0; j<b[i]; ++j) // loop through number of repetitions at b[i]
-//      {
-//        blink(a[i]); // blink the LED using duration at a[i]
-//      }
-//      ++i; // increment counter
-//    } while(i<sizeof(a)/sizeof(a)[0]); // break loop when counter = num of array elems
-
-  blinkOn(100);
-  blinkOff(500);
-  blinkOn(200);
-  blinkOff(300);
+  int a[3] = {100,250,500}; // array of blink duration values
+  int b[3] = {5, 3, 1};     // array of blink repetition values
   
+  int i=0; // initialize counter at 0
+  do
+    {
+      for (int j=0; j<b[i]; ++j) // loop through number of repetitions at b[i]
+      {
+        blink(a[i]); // blink the LED using duration at a[i]
+      }
+      ++i; // increment counter
+    } while(i<sizeof(a)/sizeof(a)[0]); // break loop when counter = num of array elems
 }
