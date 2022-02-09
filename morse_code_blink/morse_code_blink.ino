a3c8e4997b488e590f9867285e531cfd554b6712
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-02-09 13:19:31 -0500

    Added new project:Morse code blink

--- morse_code_blink/morse_code_blink.ino
+++ morse_code_blink/morse_code_blink.ino
@@ -27,6 +27,17 @@ void const blink(const int d1, const int d2) // Overloaded function: programmabl
   blinkOff(d2);
 }
 
+// LETTER BLINK (in progress)
+//void const blink(const int* letter)
+//{
+//  int ctr = 0;
+//  do
+//  {
+//    blink(&letter[ctr]);
+//    ++ctr;
+//  } while (ctr<sizeof(&letter)/sizeof(&letter[0]));
+//}
+
 void loop() {
   const int a[3] = {100,250,500}; // array of blink duration values
   const int b[3] = {5, 3, 1};     // array of blink repetition values
@@ -40,4 +51,14 @@ void loop() {
       }
       ++i; // increment counter
     } while(i<sizeof(a)/sizeof(a)[0]); // break loop when counter = num of array elems
+
+
+  // A in morse code is . - (a short blink, a short pause, a long blink, a long pause)
+  //  const int sBlink = 100;
+  //  const int lBlink = 300;
+  //
+  //  const int A[2] = {sBlink,lBlink}; // define const A with morse code blinks
+  //  const int B[3] = {lBlink, sBlink, sBlink};
+  //  int ctr = 0;
+  //  blink(*A);
 }
