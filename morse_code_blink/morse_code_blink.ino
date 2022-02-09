dfd732f936771ba3968095d69621e91851e67d3a
Daniel Pereira <danielpaulpereira@protonmail.com>
2022-02-09 14:15:48 -0500

    Working on letterblink

--- morse_code_blink/morse_code_blink.ino
+++ morse_code_blink/morse_code_blink.ino
@@ -28,37 +28,25 @@ void const blink(const int d1, const int d2) // Overloaded function: programmabl
 }
 
 // LETTER BLINK (in progress)
-//void const blink(const int* letter)
-//{
-//  int ctr = 0;
-//  do
-//  {
-//    blink(&letter[ctr]);
-//    ++ctr;
-//  } while (ctr<sizeof(&letter)/sizeof(&letter[0]));
-//}
+void const blink(const int* letter, const int size)
+{
+  int ctr = 0;
+  do
+  {
+    int d = letter[ctr]; 
+    blink(d);
+    ++ctr;
+  } while (ctr<size);
+}
 
 void loop() {
-  const int a[3] = {100,250,500}; // array of blink duration values
-  const int b[3] = {5, 3, 1};     // array of blink repetition values
-  
-  int i=0; // initialize counter at 0
-  do
-    {
-      for (int j=0; j<b[i]; ++j) // loop through number of repetitions at b[i]
-      {
-        blink(a[i]); // blink the LED using duration at a[i]
-      }
-      ++i; // increment counter
-    } while(i<sizeof(a)/sizeof(a)[0]); // break loop when counter = num of array elems
-
-
   // A in morse code is . - (a short blink, a short pause, a long blink, a long pause)
-  //  const int sBlink = 100;
-  //  const int lBlink = 300;
-  //
-  //  const int A[2] = {sBlink,lBlink}; // define const A with morse code blinks
-  //  const int B[3] = {lBlink, sBlink, sBlink};
-  //  int ctr = 0;
-  //  blink(*A);
+    const int sBlink = 100;
+    const int lBlink = 300;
+  
+    const int A[2] = {sBlink,lBlink}; // define const A with morse code blinks
+    const int B[3] = {lBlink, sBlink, sBlink};
+
+    blink(*A, sizeof(A)/sizeof(A[0]));
+    // blink(*B);
 }
